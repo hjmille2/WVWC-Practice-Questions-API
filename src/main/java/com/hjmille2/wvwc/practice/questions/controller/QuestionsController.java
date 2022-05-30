c8521ca3a7688fc3e97deecacaac911c1ffa5ad6
Holly Miller <miller.hj.2017@wvwc.edu>
2022-05-30 12:54:08 -0400

    added PUT multiple choice

--- src/main/java/com/hjmille2/wvwc/practice/questions/controller/QuestionsController.java
+++ src/main/java/com/hjmille2/wvwc/practice/questions/controller/QuestionsController.java
@@ -54,17 +54,17 @@ public class QuestionsController {
     //     return ResponseEntity.ok().body(questions); 
     // }
 
-    // @GetMapping("/questions/classes")
-    // public ResponseEntity<?> getAllClasses(){
-    //     List<String> classes = questionsRepository.getAllClasses();
-    //     return ResponseEntity.ok().body(classes); 
-    // }
+    @GetMapping("/questions/classes")
+    public ResponseEntity<?> getAllClasses(){
+        List<String> classes = questionsRepository.getAllClasses();
+        return ResponseEntity.ok().body(classes); 
+    }
 
-    // @GetMapping("/questions/question_types")
-    // public ResponseEntity<?> getAllQuestionTypes(){
-    //     List<String> questionTypes = questionsRepository.getAllQuestionTypes();
-    //     return ResponseEntity.ok().body(questionTypes); 
-    // }
+    @GetMapping("/questions/question_types")
+    public ResponseEntity<?> getAllQuestionTypes(){
+        List<String> questionTypes = questionsRepository.getAllQuestionTypes();
+        return ResponseEntity.ok().body(questionTypes); 
+    }
 
     @PostMapping("/questions")
     public ResponseEntity<?> createNewMultipleChoice(@RequestBody Question request){ 
@@ -72,30 +72,14 @@ public class QuestionsController {
 
         return ResponseEntity.ok().body(questionResp); 
     }
-
-    // // @PostMapping("/questions/short_ans")
-    // // public ResponseEntity<?> createNewShortAns(@RequestBody ShortAnswerRequest request){
-    // //     ShortAnswerRequest shortAnsRequest = questionsService.createShortAnswerQuestions(request); 
-
-    // //     return ResponseEntity.ok().body(shortAnsRequest); 
-    // // }
     
 
-    // @PutMapping("/questions/{question_id}")
-    // public ResponseEntity<Questions> updateQuestion(
-    //     @PathVariable(value="question_id") Long question_id, @Valid @RequestBody Questions questionDetails
-    // ) throws ResourceNotFoundException{
-    //     Questions question = questionsRepository.findById(question_id).orElseThrow(
-    //         () -> new ResourceNotFoundException("Question not found on :: " + question_id)
-    //     ); 
+    @PutMapping("/questions/{question_id}")
+    public ResponseEntity<Question> updateQuestion(
+        @PathVariable(value="question_id") Long question_id, @Valid @RequestBody Question questionDetails
+    ) throws ResourceNotFoundException{
+        Question updatedQuestion = questionService.updateQuestion(question_id, questionDetails); 
 
-    //     question.setQuestion_type(questionDetails.getQuestion_type()); 
-    //     question.setQuestion(questionDetails.getQuestion());
-    //     question.setQuestion_category(questionDetails.getQuestion_category());
-    //     question.setQuestion_class(questionDetails.getQuestion_class());
-
-    //     final Questions updatedQuestion = questionsRepository.save(question); 
-
-    //     return ResponseEntity.ok(updatedQuestion); 
-    // }
+        return ResponseEntity.ok(updatedQuestion); 
+    }
 }
--- src/main/java/com/hjmille2/wvwc/practice/questions/service/QuestionServiceImpl.java
+++ src/main/java/com/hjmille2/wvwc/practice/questions/service/QuestionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hjmille2.wvwc.practice.questions.service;
 
+import com.hjmille2.wvwc.practice.questions.exceptions.ResourceNotFoundException;
 import com.hjmille2.wvwc.practice.questions.model.MultipleChoice;
 import com.hjmille2.wvwc.practice.questions.model.Question;
 import com.hjmille2.wvwc.practice.questions.repository.QuestionRepository;
@@ -26,5 +27,42 @@ public class QuestionServiceImpl implements QuestionService{
         MultipleChoice multChoice = question.getMult_choice(); 
         multChoice.setQuestion(question); 
         return questionRepository.save(question);
+    }
+
+    public Question updateQuestion(Long questionId, Question newQuestionDetails)
+        throws ResourceNotFoundException{
+        Question question = questionRepository.findById(questionId)
+        .map( q -> {
+            if(q.getQuestion_type().equals("mult_choice")){
+                return updateMultipleChoice(q, newQuestionDetails);
+            } 
+            else {
+                //will be changed in future with short ans
+                return q; 
+            }
+        })
+        .orElseThrow(
+            () -> new ResourceNotFoundException("Question not found on :: " + questionId)
+        ); 
+
+        return question; 
+    }
+
+    public Question updateMultipleChoice(Question question, Question newQuestionDetails){
+        //update question
+        question.setQuestion(newQuestionDetails.getQuestion()); 
+        question.setQuestion_category(newQuestionDetails.getQuestion_category()); 
+        question.setQuestion_class(newQuestionDetails.getQuestion_class()); 
+        question.setQuestion_type(newQuestionDetails.getQuestion_type()); 
+
+        MultipleChoice multChoice = question.getMult_choice(); 
+        multChoice.setQuestion(question);
+        multChoice.setCorrect_ans(newQuestionDetails.getMult_choice().getCorrect_ans()); 
+        multChoice.setExplanation(newQuestionDetails.getMult_choice().getExplanation());
+        multChoice.setOpt_1(newQuestionDetails.getMult_choice().getOpt_1()); 
+        multChoice.setOpt_2(newQuestionDetails.getMult_choice().getOpt_2()); 
+        multChoice.setOpt_3(newQuestionDetails.getMult_choice().getOpt_3()); 
+
+        return questionRepository.save(question); 
     }
 }
